e1e3dea0f463f7b0386ba855e5c176ac4b568ae9
Gufdoor <gabriel.anjos.1280572@sga.pucminas.br>
2023-10-19 23:20:01 -0300

    FEAT: refactoring server-client algorithm

--- client.c
+++ client.c
@@ -8,17 +8,59 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <assert.h>
+#include <fcntl.h>
 
 #define PORT 3490
-#define MAXSIZE 1024
+
+static atomic_bool rx_should_run;
+static atomic_bool client_should_run;
+static int socket_fd;
+
+void *rx_thread_main(void *arg)
+{
+    char buffer[1024];
+    
+    while (!rx_should_run) {
+    
+    }
+    
+    while (rx_should_run) {
+        const int read = recv(socket_fd, buffer, sizeof(buffer), 0);
+         if (read > 0) {
+            buffer[read] = 0;
+            puts(buffer);
+        } else if (read < 0 && errno != EWOULDBLOCK) {
+            rx_should_run = false;
+        }
+        usleep(10 * 1000);
+    }
+    
+    return NULL;
+}
+
+static void signal_handler(int i)
+{
+    client_should_run = false;
+}
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in server_info;
-    struct hostent *he;
-    int socket_fd,num;
+    client_should_run = true;
+    rx_should_run = false;
+    
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    act.sa_handler = signal_handler;
+    assert(sigaction(SIGINT, &act, NULL) != -1);
+    
+    int num;
+    
     char buffer[1024];
-
     char buff[1024];
 
     if (argc != 2) {
@@ -26,50 +68,70 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if ((he = gethostbyname(argv[1]))==NULL) {
+    struct hostent *he = gethostbyname(argv[1]);
+    if (!he) {
         fprintf(stderr, "Nao foi possivel pegar o nome do host\n");
         exit(1);
     }
 
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
         fprintf(stderr, "Socket Failure!!\n");
         exit(1);
     }
-
+    
+    struct sockaddr_in server_info;
     memset(&server_info, 0, sizeof(server_info));
     server_info.sin_family = AF_INET;
     server_info.sin_port = htons(PORT);
     server_info.sin_addr = *((struct in_addr *)he->h_addr);
-    if (connect(socket_fd, (struct sockaddr *)&server_info, sizeof(struct sockaddr))<0) {
-        //fprintf(stderr, "Connection Failure\n");
+    
+    pthread_t rx_thread;
+    if (pthread_create(&rx_thread, NULL, rx_thread_main, NULL) < 0) {
+        perror("pthread_create");
+        exit(1);
+    }
+    
+    if (connect(socket_fd, (struct sockaddr *)&server_info, sizeof(struct sockaddr)) < 0) {
         perror("connect");
         exit(1);
     }
-
-    //buffer = "Hello World!! Lets have fun\n";
-    //memset(buffer, 0 , sizeof(buffer));
-    while(1) {
-        printf("Digite uma mensagem:\n");
-        fgets(buffer,MAXSIZE-1,stdin);
-        if ((send(socket_fd,buffer, strlen(buffer),0))== -1) {
-                fprintf(stderr, "Failure Sending Message\n");
-                close(socket_fd);
-                exit(1);
-        }
-        else {
-                // printf("Client:Message being sent: %s\n",buffer);
-                num = recv(socket_fd, buffer, sizeof(buffer),0);
-                if ( num <= 0 )
-                {
-                        printf("Either Connection Closed or Error\n");
-                        //Break from the While
-                        break;
-                }
-
-                buff[num] = '\0';
-                printf("\nMensagem recebida do servidor:  %s\n",buffer);
-           }
+    
+    int flags = fcntl(socket_fd, F_GETFL, 0);
+    if (flags < 0) {
+        perror("fcntl");
+        exit(1);
     }
-    close(socket_fd);   
-
+    
+    flags |= O_NONBLOCK;
+    
+    flags = fcntl(socket_fd, F_SETFL, flags);
+    if (flags < 0) {
+        perror("fcntl");
+        exit(1);
+    }
+    
+    rx_should_run = true;
+    while (client_should_run) {
+        printf("Digite uma mensagem:\n");
+        fgets(buffer, sizeof(buffer) - 1, stdin);
+        
+        if (send(socket_fd, buffer, strlen(buffer), 0) == -1) {
+            fprintf(stderr, "Failure Sending Message\n");
+            client_should_run = false;
+            continue;
+        } 
+        
+        if (!rx_should_run) {
+            // If this flag is false, recv has failed.
+            client_should_run = false;
+        }
+    }
+    
+    rx_should_run = false;
+    pthread_join(rx_thread, NULL);
+    
+    close(socket_fd);
+    return 0;   
 }
+
--- server.c
+++ server.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,87 +10,178 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#include <signal.h>
 
 #define PORT 3490
 #define BACKLOG 10
-#define MAXSIZE 1024
+#define MAX_CONNECTIONS 4 
 
-int main()
+struct connection {
+    int client_socket;
+    bool is_used;
+};
+
+static atomic_bool server_should_run;
+static atomic_bool chat_should_run;
+
+static pthread_mutex_t connection_mutex;
+static struct connection connections[MAX_CONNECTIONS];
+
+static void add_client(int socket)
 {
-    struct sockaddr_in server;
-    struct sockaddr_in dest;
-    int status,socket_fd, client_fd,num;
-    socklen_t size;
+    for (int i = 0; i < MAX_CONNECTIONS; ++i) {
+        if (!connections[i].is_used) {
+            connections[i].is_used = true;
+            connections[i].client_socket = socket;
+            break;
+        }
+    }
+}
 
-    char buffer[10241];
-    char *buff;
-//  memset(buffer,0,sizeof(buffer));
-    int yes =1;
+static void remove_client(int socket)
+{
+    for (int i = 0; i < MAX_CONNECTIONS; ++i) {
+        if (connections[i].client_socket == socket) {
+            connections[i].is_used = false;
+            connections[i].client_socket = 0;
+            break;
+        }
+    }
+}
 
+static void signal_handler(int i)
+{
+    server_should_run = false;
+}
 
+static void broadcast(int from_client_index, const char* message, int length)
+{
+    for (int i = 0; i < MAX_CONNECTIONS; ++i) {
+        if (i == from_client_index) {
+            continue;
+        } 
+        send(connections[i].client_socket, message, length, 0);
+    }
+}
 
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
+void *chat_thread_main(void *arg)
+{
+    char buffer[1024];
+ 
+    while (!chat_should_run) {
+        
+    }
+    
+    while (chat_should_run) {
+        pthread_mutex_lock(&connection_mutex);
+        
+        for (int i = 0; i < MAX_CONNECTIONS; ++i) {
+            if (!connections[i].is_used) {
+                continue;
+            }
+            
+            struct connection* con = &connections[i];
+            const int read = recv(con->client_socket, buffer, sizeof(buffer), 0);
+            
+            if (read < 0 && errno != EWOULDBLOCK) {
+                shutdown(con->client_socket, SHUT_RDWR);
+                close(con->client_socket);
+                remove_client(con->client_socket);
+                fputs("removed client\n", stderr);
+            } else if (read > 0) {
+                buffer[read] = 0;
+                puts(buffer);
+                broadcast(i, buffer, read);
+            }
+        }
+        
+        pthread_mutex_unlock(&connection_mutex);
+        usleep(10 * 1000);
+    }
+    
+    return NULL;
+}
+
+int main(void) {
+    server_should_run = true;
+    chat_should_run = false;
+    
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    act.sa_handler = signal_handler;
+    assert(sigaction(SIGINT, &act, NULL) != -1);
+    
+    memset(&connections, 0, sizeof(connections));
+    
+    const int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
         fprintf(stderr, "falha no socket!!\n");
         exit(1);
     }
-
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
-    memset(&server, 0, sizeof(server));
-    memset(&dest,0,sizeof(dest));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    server.sin_addr.s_addr = INADDR_ANY; 
-    if ((bind(socket_fd, (struct sockaddr *)&server, sizeof(struct sockaddr )))== -1)    { //sizeof(struct sockaddr) 
+    
+    struct sockaddr_in server_adr;
+    memset(&server_adr, 0, sizeof(server_adr));
+    server_adr.sin_family =  AF_INET;
+    server_adr.sin_port = htons(PORT);
+    server_adr.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(socket_fd, (struct sockaddr *)&server_adr, sizeof(struct sockaddr )) < 0) { 
         fprintf(stderr, "falha no bind\n");
         exit(1);
     }
 
-    if ((listen(socket_fd, BACKLOG))== -1){
+    if (listen(socket_fd, BACKLOG) < 0) {
         fprintf(stderr, "falha ao criar servidor\n");
         exit(1);
     }
-
-    while(1) {
-
-        size = sizeof(struct sockaddr_in);
-
-        if ((client_fd = accept(socket_fd, (struct sockaddr *)&dest, &size))==-1 ) {
-            perror("accept");
-            exit(1);
+    
+    pthread_t chat_thread;
+    if (pthread_create(&chat_thread, NULL, chat_thread_main, NULL) < 0) {
+        exit(1);
+    }
+    
+    if (pthread_mutex_init(&connection_mutex, NULL) < 0) {
+        exit(1);
+    }
+    
+    chat_should_run = true;
+    while (server_should_run) {
+        struct sockaddr_in client_adr;
+        socklen_t size = sizeof(client_adr);
+        memset(&client_adr, 0, sizeof(client_adr));
+        
+        const int client_fd = accept4(socket_fd, (struct sockaddr *)&client_adr, &size, SOCK_NONBLOCK);
+        if (client_fd < 0) {
+            perror("accept4");
+            continue;
         }
-        printf("Conectado ao cliente %s\n", inet_ntoa(dest.sin_addr));
-
-        while(1) {
-
-                if ((num = recv(client_fd, buffer, 1024,0))== -1) {
-                        perror("recv");
-                        exit(1);
-                }
-                else if (num == 0) {
-                        printf("conexao terminada\n");
-                        //So I can now wait for another client
-                        break;
-                }
-                buffer[num] = '\0';
-                printf("\nMensagem recebida do cliente: %s\n", buffer);
-                printf("Digite uma mensagem:\n");
-                fgets(buffer,MAXSIZE-1,stdin);
-                if ((send(client_fd,buffer, strlen(buffer),0))== -1) 
-                {
-                     fprintf(stderr, "Erro ao mandar mensagem\n");
-                     close(client_fd);
-                     break;
-                }
-
-                // printf("Server:Msg being sent: %s\nNumber of bytes sent: %ld\n",buffer, strlen(buffer));
-
-        } //End of Inner While...
-        //Close Connection Socket
-        close(client_fd);
-    } //Outer While
+        
+        pthread_mutex_lock(&connection_mutex);
+        add_client(client_fd);
+        pthread_mutex_unlock(&connection_mutex);
+        
+        printf("Conectado ao cliente %s\n", inet_ntoa(client_adr.sin_addr));
+        usleep(10 * 1000);
+    }
+    
+    chat_should_run = false;
+    pthread_join(chat_thread, NULL);
+    
+    //struct sockaddr_in dest;
+    //int status, client_fd;
+    
+#if 0 
+    int yes =1;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+#endif 
+    shutdown(socket_fd, SHUT_RDWR);
     close(socket_fd); 
     return 0;
 }
